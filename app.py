c3f9552d0188f969c272f21123456730147099bd
GatlaSowmya <124651078+GatlaSowmya@users.noreply.github.com>
2025-06-14 14:22:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     version = os.getenv('APP_VERSION', 'Unknown')
-    return f"✅ Welcome visys cloud technologies!{version}"
+    return f"✅ Welcome visys cloud technologies sowmya!{version}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
