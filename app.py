a1c29e25329700cd0866442e31425fc9ae53787d
GatlaSowmya <124651078+GatlaSowmya@users.noreply.github.com>
2025-06-14 15:00:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 import os
 
 app = Flask(__name__)
+DATA_FILE = "/data/store.txt"
+VERSION = os.getenv("APP_VERSION", "Unknown")
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    version = os.getenv('APP_VERSION', 'Unknown')
-    return f"✅ Welcome visys cloud technologies sowmya!{version}"
+    if request.method == 'POST':
+        message = request.form.get("message", "")
+        with open(DATA_FILE, "a") as f:
+            f.write(f"{message}\n")
+        return f"✅ Message saved in container {VERSION}"
+
+    elif request.method == 'GET':
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE) as f:
+                content = f.read()
+        else:
+            content = "No data yet"
+        return f"<h3>Container {VERSION} Data:</h3><pre>{content}</pre>" + """
+            <form method="POST">
+                <input name="message" placeholder="Enter message" />
+                <input type="submit" />
+            </form>
+        """
 
 if __name__ == '__main__':
+    os.makedirs("/data", exist_ok=True)
     app.run(host='0.0.0.0', port=5000)
-
-
-
