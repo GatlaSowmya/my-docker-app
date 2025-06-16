9d1bf24276e63b7d297b17bf275ef1349d1af825
GatlaSowmya <124651078+GatlaSowmya@users.noreply.github.com>
2025-06-16 10:07:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,12 @@
-from flask import Flask, request
+from flask import Flask
 import os
 
 app = Flask(__name__)
+version = os.environ.get("APP_VERSION", "v0")
 
-VERSION = os.environ.get("APP_VERSION", "1")
-DATA_FILE = "/data/data.txt"
+@app.route("/")
+def home():
+    return f"✅ Welcome visys cloud technologies-1! - {version}"
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        msg = request.form.get('message', '')
-        with open(DATA_FILE, 'a') as f:
-            f.write(f"{msg}\n")
-        return f"✅ Data saved in Container {VERSION}"
-
-    elif request.method == 'GET':
-        if os.path.exists(DATA_FILE):
-            with open(DATA_FILE) as f:
-                content = f.read()
-        else:
-            content = "No data yet."
-
-        label = "Old" if VERSION == "1" else "New"
-        return f"""
-        <h2>Container {VERSION} ({label}) Data:</h2>
-        <pre>{content}</pre>
-        <form method='POST'>
-            <input name='message' placeholder='Enter message' />
-            <button type='submit'>Submit</button>
-        </form>
-        """
-        
-if __name__ == '__main__':
-    os.makedirs('/data', exist_ok=True)
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
